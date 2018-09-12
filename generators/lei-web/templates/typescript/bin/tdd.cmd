0f46d4050aab8c9a10df9296e059972ec94ff73f
YourtionGuo <yourtion@gmail.com>
2018-09-12 13:30:56 +0800

    :sparkles: feat: add mocha -b bail after first test failure

--- generators/lei-web/templates/typescript/bin/tdd.cmd
+++ generators/lei-web/templates/typescript/bin/tdd.cmd
@@ -3,4 +3,4 @@ cd $(pwd)
 set NODE_ENV=test
 set DEBUG=erest:test*
 set LOG=true
-mocha  -r ts-node/register --watch-extensions ts --watch test/api/test-$1.ts
+mocha -b -r ts-node/register --watch-extensions ts --watch test/api/test-$1.ts
