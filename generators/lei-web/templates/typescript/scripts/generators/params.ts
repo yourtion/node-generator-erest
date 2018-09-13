53eb5c61b294dc4e1e22fb25f45342d08d7f1358
YourtionGuo <yourtion@gmail.com>
2018-09-13 11:22:00 +0800

    :bug: fix: schemaToInterface

--- generators/lei-web/templates/typescript/scripts/generators/params.ts
+++ generators/lei-web/templates/typescript/scripts/generators/params.ts
@@ -4,7 +4,7 @@ import { firstUpperCase, underscore2camelCase } from "../../src/global/base/util
 
 function schemaToInterface(name: string, schema: ISchemaType, isRequired = false) {
   const required = schema.required || isRequired;
-  const type = apiService.type.get(schema.type.info || schema.type);
+  const type = apiService.type.get(schema.type).info;
   let teType = type.tsType || "any";
   const res = [];
   if (schema.comment) {
