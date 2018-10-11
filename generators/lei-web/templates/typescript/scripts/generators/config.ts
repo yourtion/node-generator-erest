342d95bf72ed414e5b38c5da566bd5e9bbdd5330
YourtionGuo <yourtion@gmail.com>
2018-10-11 17:38:24 +0800

    :bug: fix config

--- generators/lei-web/templates/typescript/scripts/generators/config.ts
+++ generators/lei-web/templates/typescript/scripts/generators/config.ts
@@ -11,19 +11,61 @@ import { firstUpperCase } from "../../src/global/base/utils";
 const result = [];
 const conf: Record<string, string> = {};
 
+/** 获取配置名 */
+function confName(sco, key, n = 1) {
+  return sco ? `${sco}${"^".repeat(n)}${firstUpperCase(key)}` : firstUpperCase(key);
+}
+
+/** 往上n级 */
+function splitLast(scope, n = 1) {
+  return scope
+    .split("^")
+    .slice(0, -1 * n)
+    .join("^");
+}
+
 function getConfigComment() {
   const configFile = fs.readFileSync(path.resolve(__dirname, "../../config/base.yaml"));
   const strs = configFile.toString().split("\n");
+  let scope = "";
+  let ll = 0;
+  let comm = "";
   for (const line of strs) {
-    const keys = line.split(":", 1);
+    const keys = line.split(":");
     const key = keys && keys[0];
-    const comments = line.split("#");
-    const comment = comments && comments.length > 1 && comments[1];
-    if (comment && key[0] !== " ") {
-      conf[key] = comment.trim();
+    const commentIdx = line.indexOf("#");
+    const keyIdx = line.indexOf(":");
+    const comment = commentIdx > -1 && line.substring(commentIdx + 1);
+    if (keys.length < 2) {
+      if (comment && (keyIdx < 0 || keyIdx < commentIdx)) comm = comment;
+      continue;
+    }
+    const rKey = key.trim();
+    const l = key.length - rKey.length;
+    if (l === 0) {
+      // 处理第一级情况
+      scope = confName("", rKey);
+    } else if (l > ll) {
+      // 深入n层
+      scope = confName(scope, rKey, (l - ll) / 2);
+    } else if (l <= ll) {
+      // 同级或退出n层
+      scope = confName(splitLast(scope, (ll - l) / 2 + 1), rKey);
+    }
+    ll = l;
+    if (comment || comm) {
+      const c = comment || comm;
+      conf[scope.replace(/\^/g, "")] = c.trim();
+      if (comm) comm = "";
     }
   }
+  // console.log(conf);
   return conf;
+}
+
+function getName(name: string, k: string) {
+  if (name === "config") return firstUpperCase(k);
+  return firstUpperCase(name) + firstUpperCase(k);
 }
 
 function toInterface(obj, name) {
@@ -33,24 +75,21 @@ function toInterface(obj, name) {
     res.push("[key: string]: any;");
   }
   for (const k in obj) {
-    if (name === "config" && conf[k]) {
-      res.push(`/** ${conf[k]} */`);
+    // console.log(name, k, getName(name, k), conf[getName(name, k)]);
+    if (conf[getName(name, k)]) {
+      res.push(`/** ${conf[getName(name, k)]} */`);
     }
     if (typeof obj[k] === "object") {
       if (Array.isArray(obj[k])) {
-        if (name === "config") {
-          res.push(`  ${k}: I${firstUpperCase(k)}[];`);
-          toInterface(obj[k][0], k);
+        if (typeof obj[k][0] === "object") {
+          res.push(`  ${k}: I${getName(name, k)}[];`);
+          toInterface(obj[k][0], getName(name, k));
         } else {
           res.push(`  ${k} : ${typeof obj[k][0]}[];`);
         }
       } else {
-        if (name === "config") {
-          res.push(`  ${k}: I${firstUpperCase(k)};`);
-          toInterface(obj[k], k);
-        } else {
-          res.push(`  ${k} : ${typeof obj[k]};`);
-        }
+        res.push(`  ${k}: I${getName(name, k)};`);
+        toInterface(obj[k], getName(name, k));
       }
     } else {
       res.push(`  ${k} : ${typeof obj[k]};`);
