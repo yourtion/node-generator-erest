89bd419e170495426ba97a38796ca7bf31857755
YourtionGuo <yourtion@gmail.com>
2018-12-13 13:39:26 +0800

    :bug:  fix: help gen

--- generators/index.js
+++ generators/index.js
@@ -6,7 +6,7 @@ module.exports = class extends Generator {
   }
 
   help() {
-    this.log('使用 `erest:init lei-web` 初始化项目');
+    this.log('使用 `yo erest:lei-web` 初始化项目');
     this.log('使用 `yo erest:code service xxx 测试` 初始化 Service');
     this.log('使用 `yo erest:code router xxx` 初始化 Router');
   }
