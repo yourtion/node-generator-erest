49c8c73be50a42e65e3722af3cf78ddf5b6f60ff
YourtionGuo <yourtion@gmail.com>
2018-12-15 23:13:55 +0800

    :sparkles: feat(libs): add jwt

---
+++ generators/libs/templates/typescript/jwt.ts
@@ -0,0 +1,35 @@
+import jsonwebtoken from "jsonwebtoken";
+import { Context, component } from "../web";
+
+export const cookie = component.cookieParser();
+
+function parseLoginTokenRaw(ctx: Context) {
+  const token = ctx.request.cookies.login_token;
+  if (token) {
+    const ret = jsonwebtoken.verify(token, ctx.config.sessionSecret);
+    if (typeof ret !== "string") {
+      ctx.user = ret as any;
+    }
+  }
+  ctx.next();
+}
+
+function ensureLoginRaw(ctx: Context) {
+  if (ctx.user && ctx.user.id > 0 && ctx.user.openid) {
+    return ctx.next();
+  }
+  throw new ctx.errors.PermissionsError("not login");
+}
+
+/** 当有可能登录也有可能未登录的情况使用 */
+export const parseLoginToken = [cookie, parseLoginTokenRaw];
+
+/** 强制一定要登录的情况使用 */
+export const ensureLogin = [...parseLoginToken, ensureLoginRaw];
+
+/** 签发Token */
+export function signToken(ctx: Context, id: number, openid: string) {
+  const token = jsonwebtoken.sign({ id, openid }, ctx.config.sessionSecret);
+  ctx.response.cookie("login_token", token, { path: "/", maxAge: ctx.config.cookieMaxAge });
+  return token;
+}
