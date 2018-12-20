b198930535445fcd48f92f57c012a3839b9c0cb6
YourtionGuo <yourtion@gmail.com>
2018-12-20 15:22:26 +0800

    :sparkles: feat: add tdd info

--- generators/index.js
+++ generators/index.js
@@ -9,5 +9,6 @@ module.exports = class extends Generator {
     this.log('使用 `yo erest:lei-web` 初始化项目');
     this.log('使用 `yo erest:code service xxx 测试` 初始化 Service');
     this.log('使用 `yo erest:code router xxx` 初始化 Router');
+    this.log('使用 `yo erest:libs` 添加 Lib 插件');
   }
 };
--- generators/lei-web/templates/typescript/bin/tdd.sh
+++ generators/lei-web/templates/typescript/bin/tdd.sh
@@ -4,4 +4,5 @@ cd $(pwd)
 
 export NODE_ENV=test
 export LOG=true
+export DEBUG=erest:test*
 ./node_modules/.bin/mocha --watch-extensions ts --watch test/api/test-$1.ts
