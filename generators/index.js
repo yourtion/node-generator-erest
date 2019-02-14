ac675aecdc8367f1420068cce7c5f8031c9250f0
YourtionGuo <yourtion@gmail.com>
2019-02-14 14:43:45 +0800

    :sparkles: feat: show version on help

--- generators/index.js
+++ generators/index.js
@@ -1,4 +1,5 @@
 const Generator = require('yeoman-generator');
+const pkg = require('../package.json');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -6,6 +7,7 @@ module.exports = class extends Generator {
   }
 
   help() {
+    this.log(`${pkg.name}@${pkg.version}`);
     this.log('使用 `yo erest:lei-web` 初始化项目');
     this.log('使用 `yo erest:code service xxx 测试` 初始化 Service');
     this.log('使用 `yo erest:code router xxx` 初始化 Router');
