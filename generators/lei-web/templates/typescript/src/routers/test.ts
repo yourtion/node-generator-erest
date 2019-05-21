312164bd727cd9093f10306554f11762be3316b3
YourtionGuo <yourtion@gmail.com>
2019-05-21 15:26:41 +0800

    :bug: fix: test bug

--- generators/lei-web/templates/typescript/src/routers/test.ts
+++
@@ -1,9 +0,0 @@
-import apiService from "../api";
-const api = apiService.group("Base");
-
-api
-  .get("/index")
-  .title("测试Index")
-  .register(ctx => {
-    ctx.response.ok("Hello, API Framework Index");
-  });
